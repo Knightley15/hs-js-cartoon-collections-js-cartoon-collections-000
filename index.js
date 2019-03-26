c693b7155146fc3eef182989cc6a6faca4d49ede
Juliette Knightley <knightleyjb@gmail.com>
2019-03-26 14:03:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  var list = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    list.push(`${i + 1}. ${dwarves[i]}`);
+  }
+  return `${list.join(' ')} `;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+	return planeteerCalls.map(function(ele) {
+		return ele.toUpperCase() + "!";
+	});
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  var cheese = "cheddar, gouda, camembert";
+  for(let i = 0; i < foods.length; i++) {
+    var cheeseIndex = cheese.indexOf(foods[i]); 
+      if (cheeseIndex !== -1) {
+         return foods[i];
+      } 
+    }
+   
+   return "no cheese!";
 }
+  
